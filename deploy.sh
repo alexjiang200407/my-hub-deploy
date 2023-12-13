542ca0a53ee358b648a69d770e3e08558fc71e2a
alex jiang <alexjiang201507@gmail.com>
2023-12-13 14:29:17 +1100

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+# Setup ansible
+sudo hostnamectl set-hostname AnsibleMgmtNode
+sudo apt-get update
+sudo apt install python3-pip -y
+sudo pip3 install --upgrade pip
+pip3 install "ansible==2.9.17"
+pip3 install ansible[azure]
+sudo apt install ansible -y
+
+# Install dependencies for ansible
+ansible-galaxy collection install community.general
+ansible-galaxy collection install community.mysql
+
+# Run ansible
+ansible-playbook main.yml -i inventory
+
